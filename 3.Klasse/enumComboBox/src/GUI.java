454522f395512b8e7b05f6bef5cbcfa19c319877
PatrickMagAnime <riedl.patrick1707@gmail.com>
2025-01-16 11:12:16 +0100

    meowpdate

--- 3.Klasse/enumComboBox/src/GUI.java
+++ 3.Klasse/enumComboBox/src/GUI.java
@@ -1,12 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
     private JTextField textField1;
     private JTextField textField2;
-    private JComboBox comboBox1;
     private JTextField textField3;
     private JButton ladenButton;
     private JButton suchenButton;
     private JButton speichernButton;
     private JTextArea textArea1;
-}
+    private JPanel mainPanel;
+    private JComboBox kategorieCBox;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Produktverwaltung");
+        frame.setContentPane(new GUI().mainPanel);
+        frame.setVisible(true);
+        frame.setSize(400,500);
+    }
+
+    public GUI(){
+        kategorieCBox.setModel(new DefaultComboBoxModel(Kategorien.values()));
+        suchenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                for (int i = 0; 1 < Kategorien.values().length; i++) {
+                    textArea1.append(Kategorien.values()[i].toString() + "\n");
+                }}});}}
