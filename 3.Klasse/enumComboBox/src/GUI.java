06fd885db883c60417fd6e9342a6028b54678a84
PatrickMagAnime <riedl.patrick1707@gmail.com>
2025-01-16 10:45:41 +0100

    meow

---
+++ 3.Klasse/enumComboBox/src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JTextField textField3;
+    private JButton ladenButton;
+    private JButton suchenButton;
+    private JButton speichernButton;
+    private JTextArea textArea1;
+}
