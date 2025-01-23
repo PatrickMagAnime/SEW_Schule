9933314007aa74c2c2fc4c650a4bdba378da2d2f
PatrickMagAnime <riedl.patrick1707@gmail.com>
2025-01-23 10:15:47 +0100

    meow

--- 3.Klasse/enumComboBox/src/ProduktManager.java
+++ 3.Klasse/enumComboBox/src/ProduktManager.java
@@ -1,28 +1,31 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class ProduktManager {
-    private static final String FILE_PATH = "produkt.json";
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    public static void saveProdukt(Produkt produkt) {
-        try (FileWriter writer = new FileWriter(FILE_PATH)) {
-            gson.toJson(produkt, writer);
+    public static void saveProdukte(ArrayList<Produkt> produkte, String filePath) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(produkte, writer);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static Produkt loadProdukt() {
-        try (FileReader reader = new FileReader(FILE_PATH)) {
-            return gson.fromJson(reader, Produkt.class);
+    public static ArrayList<Produkt> loadProdukte(String filePath) {
+        try (FileReader reader = new FileReader(filePath)) {
+            Type listType = new TypeToken<ArrayList<Produkt>>() {}.getType();
+            return gson.fromJson(reader, listType);
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return new ArrayList<>(); // Leere Liste zurückgeben, falls Fehler
         }
     }
 }
