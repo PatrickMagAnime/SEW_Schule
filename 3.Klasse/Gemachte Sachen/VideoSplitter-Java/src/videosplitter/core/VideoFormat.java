dab8868ecc8b3c581bfc50ea4345d518d73c17cf
PatrickMagAnime <riedl.patrick1707@gmail.com>
2025-06-07 18:54:28 +0200

    backup

--- 3.Klasse/Gemachte Sachen/VideoSplitter-Java/src/videosplitter/core/VideoFormat.java
+++
@@ -1,6 +0,0 @@
-package videosplitter.core;
-
-// enum für video datei formate
-public enum VideoFormat {
-    MP4, AVI, MOV, MKV, FLV, WMV, MPEG, MPG, WEBM, M4V, VOB, OGV, MTS, M2TS, TS, F4V, MXF, RM, SWF, DIVX, UNKNOWN
-}
